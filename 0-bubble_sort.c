0c6d3c1d74c154970465ccfaf2b3c9875c9082b7
Mesih Grmay <mesihgrmay@yahoo.com>
2022-08-16 06:42:25 -0700

    Task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort an array in ascending order
+ *@array: input array of integers
+ *@size: size of the array
+  *Return: void
+  */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				size_t temp = array[i];
+
+				array[i] = array[j];
+				array[j] = temp;
+			}
+
+		}
+
+	}
+
+}
