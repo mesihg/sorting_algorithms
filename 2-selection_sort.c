72d2ee60e6aea63a6b169de99fc25cecb1bedcec
Mesih Grmay <mesihgrmay@yahoo.com>
2022-08-16 08:56:22 -0700

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+
+/**
+ * swap - swaps the given inputs
+ *@a: integer input one
+ *@b: integer input two
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ *selection_sort - sort an array of integers using selection sort
+ *@array: array of integers
+ *@size: size of the integers
+ *Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		int min_index = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		swap(&array[min_index], &array[i]);
+		print_array(array, size);
+	}
+}
